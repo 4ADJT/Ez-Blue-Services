477da2f0beb388e7e316280c177a620112ffd0b3
Bruno Matheus <brumalones@gmail.com>
2024-07-08 20:29:33 -0300

    prototipo da entidade parking

---
+++ src/main/java/br/com/ezblue/ezblueservices/domain/parking/ParkingEntity.java
@@ -0,0 +1,46 @@
+package br.com.ezblue.ezblueservices.domain.parking;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "parking")
+@Table(name = "parking")
+public class ParkingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID clientId;
+    private UUID cityId;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
+    public ParkingEntity(UUID clientId, UUID cityId, LocalDateTime startTime, LocalDateTime endTime) {
+        this.clientId = clientId;
+        this.cityId = cityId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.createdDate = LocalDateTime.now();
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    public void updateParkingDetails(LocalDateTime endTime) {
+        this.endTime = endTime;
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+}
