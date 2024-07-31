5c78903764161fe058c5eecaad84f94ba875f56b
Bruno Matheus <brumalones@gmail.com>
2024-07-30 22:24:16 -0300

    update application

--- src/main/java/br/com/ezblue/ezblueservices/EzBlueServicesApplication.java
+++ src/main/java/br/com/ezblue/ezblueservices/EzBlueServicesApplication.java
@@ -14,23 +14,4 @@ public class EzBlueServicesApplication {
         SpringApplication.run(EzBlueServicesApplication.class, args);
     }
 
-    // TODO: +Gerenciamento de Tarifas de Estacionamento: CRUD de tarifas de
-    // estacionamento por cidade (e possivelmente por bairro).
-    // TODO: +Registro de Estacionamento: Registrar onde e por quanto tempo cada
-    // veículo ficou estacionado.
-    // TODO: +-API de Localização: API para identificar a localização onde o cliente
-    // está parando.
-
-    // Funcionalidades Adicionais:
-    // TODO: -Integração com Mapas e Geolocalização: Utilizar APIs de mapas para
-    // fornecer localização precisa e mostrar disponibilidade de vagas.
-    // TODO: -Gerenciamento de Tarifas Dinâmicas: Implementar tarifas dinâmicas
-    // baseadas na demanda, horário e localização.
-    // TODO: -Relatórios e Estatísticas: Geração de relatórios e estatísticas de uso
-    // para administração das cidades.
-    // TODO: -Integração com Serviços de Monitoramento: Interface para integrar com
-    // serviços de monitoramento de estacionamento (por exemplo, sensores de vagas).
-    // TODO: + Notificações e Alertas: Enviar notificações sobre vagas disponíveis,
-    // mudanças de tarifas, etc.
-
 }
